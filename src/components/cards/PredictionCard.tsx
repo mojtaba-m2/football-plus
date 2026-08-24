a54e7f1e980a9ea9aa388c38ec74434fc344d0df
mojtaba m2 <mojtaba1378815@gmail.com>
2026-08-24 11:50:17 +0330

    feat: add match Prediction section

---
+++ src/components/cards/PredictionCard.tsx
@@ -0,0 +1,57 @@
+import Image from "next/image";
+import Link from "next/link";
+
+function PredictionCard() {
+  return (
+    <div className="flex flex-col justify-center py-4 items-center bg-gray-800 w-full lg:w-1/2 m-auto">
+      <div className="flex items-center justify-between py-2">
+        <div className="flex items-center justify-center">
+          <div className="flex items-center">
+            <figure className="size-10 lg:size-16 relative">
+              <Image src={"/logo/realMadrid.png"} alt="realMadrid" fill />
+            </figure>
+            <h3 className="mx-2 text-base md:text-lg lg:text-xl text-white">
+              رئال مادرید
+            </h3>
+          </div>
+          <input
+            min={0}
+            max={10}
+            className="bg-gray-300 w-10 text-center mx-2"
+            type="number"
+            placeholder="گل"
+          />
+        </div>
+
+        <span className=" lg:text-2xl text-white mx-4">vs</span>
+
+        <div className="flex items-center justify-center">
+          <input
+            min={0}
+            max={10}
+            className="bg-gray-300 w-10 text-center mx-2"
+            type="number"
+            placeholder="گل"
+          />
+          <div className="flex items-center">
+            <h3 className="mx-2 text-base md:text-lg text-white">بارسلونا</h3>
+            <figure className="size-10 lg:size-16 relative">
+              <Image src={"/logo/barcelona.png"} alt="barcelona" fill />
+            </figure>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col items-center gap-2 mt-2">
+        <button className="px-2 py-1 lg:px-4 lg:py-2 bg-green-600 text-white lg:font-extrabold rounded cursor-pointer">
+          ثبت پیش بینی
+        </button>
+
+        <Link href={"#"} className="text-blue-200 lg:font-semibold">
+          برای پیش بینی بازی های بیشتر کلیک کنید
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default PredictionCard;
---
+++ src/components/home/Predictions.tsx
@@ -0,0 +1,13 @@
+import PredictionCard from "../cards/PredictionCard";
+
+function Predictions() {
+  return (
+    <div className="mt-8 md:mt-20 bg-gray-200 ">
+      <h1 className="py-8 text-xl text-center">پیش بینی</h1>
+
+      <PredictionCard />
+    </div>
+  );
+}
+
+export default Predictions;
